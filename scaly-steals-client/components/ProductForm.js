dd7f014bfcc74023d65551b13b4f94acdd7bfdfb
alanis <alanis_narvaez@icloud.com>
2024-04-24 22:56:01 -0400

    final before pres

--- scaly-steals-client/components/ProductForm.js
+++ scaly-steals-client/components/ProductForm.js
@@ -6,7 +6,7 @@ import {ReactSortable} from "react-sortablejs";
 import styled from "styled-components";
 
 const PlaceholderText = styled.div`
-    color: #A9A9AC;
+    color: #FFF;
 `;
 
 const PhotoDiv = styled.div`
@@ -19,6 +19,25 @@ const CategoryDiv = styled.div`
     select {
         color: #000;
     }
+`;
+
+const Input = styled.input`
+    padding: 0.5rem;
+    margin-bottom: 1rem;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    font-size: 1rem;
+    width: 100%;
+`;
+
+const TextArea = styled.textarea`
+    padding: 0.5rem;
+    margin-bottom: 1rem;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    font-size: 1rem;
+    width: 100%;
+    min-height: 100px; /* Set a minimum height */
 `;
 
 export default function ProductForm({
@@ -58,7 +77,7 @@ export default function ProductForm({
     }
 
     if (goToProducts) {
-        router.push('user/profile/'+user);
+        router.push('/profile'+user);
     }
 
     async function uploadImages(ev) {
@@ -84,7 +103,7 @@ export default function ProductForm({
     return (
         <form onSubmit={saveProduct}>
             <label>Product name</label>
-            <input type="text" placeholder="product name" value={title} onChange={ev => setTitle(ev.target.value)}/>
+            <Input type="text" placeholder="product name" value={title} onChange={ev => setTitle(ev.target.value)}/>
 
             <CategoryDiv>
                 <label>Category</label>
@@ -117,7 +136,7 @@ export default function ProductForm({
                     )}
                     
                     <label className="w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-primary rounded-sm bg-white shadow-sm border border-primary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#A9A9AC" className="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="25" height="25" strokeWidth={1.5} stroke="#FFF" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
                         </svg>
                         <PlaceholderText>
@@ -129,13 +148,13 @@ export default function ProductForm({
             </PhotoDiv>
 
             <label>Description</label>
-            <textarea
+            <TextArea
             placeholder="description"
             value={description}
             onChange={ev => setDescription(ev.target.value)}
             />
             <label>Price (in USD)</label>
-            <input
+            <Input
             type="number" placeholder="price"
             value={price}
             onChange={ev => setPrice(ev.target.value)}
